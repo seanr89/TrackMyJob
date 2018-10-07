d0348d75b987b75d6f5488691865bb8182d922e0
Sean <srafferty89@gmail.com>
2018-10-07 13:49:44 +0100

    working on additions to start google sign in process

--- app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
+++ app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
@@ -2,11 +2,17 @@ package com.example.sean.trackmyjob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 class GoogleSignInActivity : AppCompatActivity() {
+
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_sign_in)
+
+       mAuth = FirebaseAuth.getInstance()
     }
 }
