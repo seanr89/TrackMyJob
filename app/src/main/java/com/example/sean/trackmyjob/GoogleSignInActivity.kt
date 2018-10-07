616973bb351de22db511c62424acac8256ec8d8e
Sean <srafferty89@gmail.com>
2018-10-07 17:38:46 +0100

    Upgraded and fixed some calls during sign-in with google auth targetted

--- app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
+++ app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
@@ -12,12 +12,17 @@ import com.google.firebase.auth.FirebaseUser;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount
 import android.content.Intent
 import android.util.Log
+import android.widget.Toast
 import com.google.android.gms.tasks.Task
 import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.AuthCredential
+import com.google.firebase.auth.GoogleAuthProvider
+
 
 
 class GoogleSignInActivity : AppCompatActivity(), View.OnClickListener {
 
+    private val TAG = "GoogleSignInActivity"
     private var mAuth: FirebaseAuth? = null
     private var mGoogleSignInClient : GoogleSignInClient? = null
     private val RC_SIGN_IN = 2
@@ -61,9 +66,12 @@ class GoogleSignInActivity : AppCompatActivity(), View.OnClickListener {
     /**
      *
      */
-    private fun signOut() {
+    private fun signOut()
+    {
+        Log.d(TAG, object{}.javaClass.enclosingMethod.name)
         // sign out Firebase
         mAuth!!.signOut()
+        FirebaseAuth.getInstance().signOut()
         // sign out Google
         //Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback { updateUI(null) }
     }
@@ -74,34 +82,65 @@ class GoogleSignInActivity : AppCompatActivity(), View.OnClickListener {
     public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
+        Log.d(TAG, object{}.javaClass.enclosingMethod.name)
         // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
         if (requestCode == RC_SIGN_IN) {
             // The Task returned from this call is always completed, no need to attach
             // a listener.
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)
+                firebaseAuthWithGoogle(account)
+            }catch (e : ApiException) {
+
+            }
+            //handleSignInResult(task)
         }
     }
 
-    /**
-     *
-     */
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
-        try {
-            val account = completedTask.getResult(ApiException::class.java)
-            // Signed in successfully, show authenticated UI.
-            //updateUI(account)
-        } catch (e: ApiException) {
-            // The ApiException status code indicates the detailed failure reason.
-            // Please refer to the GoogleSignInStatusCodes class reference for more information.
-            Log.w("Yeoo", "signInResult:failed code=" + e.statusCode)
-            //updateUI(null)
-        }
+//    /**
+//     *
+//     */
+//    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>)
+//    {
+//        Log.d(TAG, object{}.javaClass.enclosingMethod.name)
+//        try {
+//            val account = completedTask.getResult(ApiException::class.java)
+//            // Signed in successfully, show authenticated UI.
+//            //updateUI(account)
+//            firebaseAuthWithGoogle(account)
+//        } catch (e: ApiException) {
+//            // The ApiException status code indicates the detailed failure reason.
+//            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+//            Log.w("Yeoo", "signInResult:failed code=" + e.statusCode)
+//            //updateUI(null)
+//        }
+//    }
 
+    fun firebaseAuthWithGoogle(account : GoogleSignInAccount?)
+    {
+        Log.d(TAG, object{}.javaClass.enclosingMethod.name)
+
+        val credential = GoogleAuthProvider.getCredential(account!!.idToken, null)
+        mAuth!!.signInWithCredential(credential)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful)
+                    {
+                        //todo
+                        Log.w(TAG, "Sign in successful")
+                        Toast.makeText(this, "Firebase Auth Complete", Toast.LENGTH_LONG).show()
+                    } else
+                    {
+                        Log.w(TAG, "SignInWithCredential:failure", task.exception)
+                        //Toast.makeText(this, "Firebase Auth Failed", Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 
     /**
-     *
+     * handle the button click events across the view
+     * @param v :
      */
     override fun onClick(v: View) {
         when (v.id) {
