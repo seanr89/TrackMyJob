9123785e3346db2c957fc10ad68f17053e79921e
seanr89 <srafferty89@gmail.com>
2018-10-07 11:36:26 +0100

    removal of duplicate implementation of files in gradle and converted to use androidx repos

--- app/src/main/java/com/example/sean/trackmyjob/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/sean/trackmyjob/ui/main/MainFragment.kt
@@ -1,6 +1,6 @@
 package com.example.sean.trackmyjob.ui.main
 
-import androidx.lifecycle.ViewModelProviders
+
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -14,7 +14,7 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    //private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View {
@@ -23,7 +23,7 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+        //viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
