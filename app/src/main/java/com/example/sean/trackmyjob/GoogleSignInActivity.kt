4d2bd0a78271ff6ec053b2a94df4d65aea6547ca
Sean <srafferty89@gmail.com>
2018-10-07 16:18:32 +0100

    Controls added from google sample apps for google firebase sign in

--- app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
+++ app/src/main/java/com/example/sean/trackmyjob/GoogleSignInActivity.kt
@@ -2,17 +2,110 @@ package com.example.sean.trackmyjob
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import android.content.Intent
+import android.util.Log
+import com.google.android.gms.tasks.Task
+import com.google.android.gms.common.api.ApiException
 
-class GoogleSignInActivity : AppCompatActivity() {
+
+class GoogleSignInActivity : AppCompatActivity(), View.OnClickListener {
 
     private var mAuth: FirebaseAuth? = null
+    private var mGoogleSignInClient : GoogleSignInClient? = null
+    private val RC_SIGN_IN = 2
+
+    override fun onStart() {
+        super.onStart()
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        //updateUI(account)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_google_sign_in)
 
        mAuth = FirebaseAuth.getInstance()
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        this.findViewById<Button>(R.id.btn_sign_in).setOnClickListener(this);
+    }
+
+    /**
+     *
+     */
+    private fun signIn() {
+        val signInIntent = mGoogleSignInClient!!.signInIntent
+        startActivityForResult(signInIntent, RC_SIGN_IN)
+    }
+
+
+    /**
+     *
+     */
+    private fun signOut() {
+        // sign out Firebase
+        mAuth!!.signOut()
+        // sign out Google
+        //Auth.GoogleSignInApi.signOut(mGoogleApiClient).setResultCallback { updateUI(null) }
+    }
+
+    /**
+     *
+     */
+    public override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            // The Task returned from this call is always completed, no need to attach
+            // a listener.
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleSignInResult(task)
+        }
+    }
+
+    /**
+     *
+     */
+    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+        try {
+            val account = completedTask.getResult(ApiException::class.java)
+            // Signed in successfully, show authenticated UI.
+            //updateUI(account)
+        } catch (e: ApiException) {
+            // The ApiException status code indicates the detailed failure reason.
+            // Please refer to the GoogleSignInStatusCodes class reference for more information.
+            Log.w("Yeoo", "signInResult:failed code=" + e.statusCode)
+            //updateUI(null)
+        }
+
+    }
+
+    /**
+     *
+     */
+    override fun onClick(v: View) {
+        when (v.id) {
+            R.id.btn_sign_in -> signIn()
+        }// ...
     }
 }
