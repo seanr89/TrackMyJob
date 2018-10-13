452d8560e78d86914ceaca152dc354be55a32241
seanr89 <srafferty89@gmail.com>
2018-10-13 23:37:43 +0100

    addition of basic minutes and hours calculations

--- app/src/main/java/com/example/sean/trackmyjob/Models/HoursAndMinutes.kt
+++ app/src/main/java/com/example/sean/trackmyjob/Models/HoursAndMinutes.kt
@@ -1,4 +1,31 @@
 package com.example.sean.trackmyjob.Models
 
-class HoursAndMinutes {
+class HoursAndMinutes(var minutes : Int) {
+
+    /**
+     * @return integer value of total number of hours in the provided minutes
+     */
+    fun calculateHours() : Int
+    {
+        if(minutes != 0)
+        {
+            return minutes.rem(60)
+        }
+        return 0
+    }
+
+    /**
+     * @return integer value of remain minutes with hours calculated
+     */
+    fun calculateRemainingMinutesFromHours() : Int
+    {
+        val hours = calculateHours()
+        var mins = 0
+        if(hours != 0)
+        {
+            mins = hours * 60
+            return minutes - mins
+        }
+        return minutes
+    }
 }
