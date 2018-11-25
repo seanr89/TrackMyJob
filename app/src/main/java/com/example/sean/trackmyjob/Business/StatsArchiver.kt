10ef25f114a8f4821c28b326f4d46ad154a39f8a
seanr89 <srafferty89@gmail.com>
2018-11-25 14:05:25 +0000

    Base implementation of evening alarm receiver and business logic for the stats archival process!!

---
+++ app/src/main/java/com/example/sean/trackmyjob/Business/StatsArchiver.kt
@@ -0,0 +1,17 @@
+package com.example.sean.trackmyjob.Business
+
+/**
+ * new class to provide feature set for clock event stats archival!
+ */
+class StatsArchiver {
+
+    private val TAG = "StatsArchiver"
+
+    /**
+     * execute param to archive monthly stats!
+     */
+    fun archiveStatsForTheMonth()
+    {
+
+    }
+}
---
+++ app/src/main/java/com/example/sean/trackmyjob/Services/EveningAlarmBroadcastReceiver.kt
@@ -0,0 +1,14 @@
+package com.example.sean.trackmyjob.Services
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+
+/**
+ * new class to provide feature set for evening message calls
+ */
+class EveningAlarmBroadcastReceiver : BroadcastReceiver() {
+    override fun onReceive(context: Context?, intent: Intent?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
